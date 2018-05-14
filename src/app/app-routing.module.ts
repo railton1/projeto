30ca893a87357955f9236f32463c6261583f482e
Railton Ramés <railton01@hotmail.com>
2018-05-14 11:14:32 -0300

    v0.2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Pagina2Component } from './pagina2/pagina2.component';
+import { Pagina1Component } from './pagina1/pagina1.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: 'pagina1', component: Pagina1Component},
+	{ path: 'pagina2', component: Pagina2Component},
+	{ path: '', redirectTo: '/pagina1', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,25 @@
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './app.component';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { AppRoutingModule } from './app-routing.module';
+import { Pagina1Component } from './pagina1/pagina1.component';
+import { Pagina2Component } from './pagina2/pagina2.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Pagina1Component,
+    Pagina2Component
   ],
   imports: [
     BrowserModule,
+    CarouselModule,
+    ButtonsModule,
     AppRoutingModule
   ],
   providers: [],
