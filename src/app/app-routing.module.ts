7b82c22ec2633619f592d18e506494fbfeeb68d8
Railton Ramés <railton01@hotmail.com>
2018-05-16 20:23:40 -0300

    v0.3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { Pagina2Component } from './pagina2/pagina2.component';
 import { Pagina1Component } from './pagina1/pagina1.component';
+import { Pagina2Component } from './pagina2/pagina2.component';
+import { Pagina3Component } from './pagina3/pagina3.component';
+import { Pagina4Component } from './pagina4/pagina4.component';
 
 const routes: Routes = [
 	{ path: 'pagina1', component: Pagina1Component},
 	{ path: 'pagina2', component: Pagina2Component},
+	{ path: 'pagina3', component: Pagina3Component},
+	{ path: 'pagina4', component: Pagina4Component},
 	{ path: '', redirectTo: '/pagina1', pathMatch: 'full' }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,17 @@ import { ButtonsModule } from 'ngx-bootstrap/buttons';
 import { AppRoutingModule } from './app-routing.module';
 import { Pagina1Component } from './pagina1/pagina1.component';
 import { Pagina2Component } from './pagina2/pagina2.component';
+import { Pagina3Component } from './pagina3/pagina3.component';
+import { Pagina4Component } from './pagina4/pagina4.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     Pagina1Component,
-    Pagina2Component
+    Pagina2Component,
+    Pagina3Component,
+    Pagina4Component
   ],
   imports: [
     BrowserModule,
